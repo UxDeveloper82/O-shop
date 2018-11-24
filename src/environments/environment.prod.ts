86693455b869779c45c4a8cd7858ffb5e17d0e08
electronicsmf <electronicsmf@aol.com>
2018-11-23 20:44:54 -0500

    First  Commint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAesHDSpf8LEEnlObtB5h4QXlAq3agS01w',
+    authDomain: 'oshop-d90ae.firebaseapp.com',
+    databaseURL: 'https://oshop-d90ae.firebaseio.com',
+    projectId: 'oshop-d90ae',
+    storageBucket: '',
+    messagingSenderId: '933303167167'
+  }
 };
+
